08ed9109a313210374293be0c9278637bc50e2a9
awam.m.wang <awam.m.wang@cn.pwc.com>
2019-08-14 14:20:19 +0800

    bug: 处理绑定值被代码修改的问题-2

--- src/index.js
+++ src/index.js
@@ -53,12 +53,13 @@ MyPlugin.install = function(Vue: Vue, outerOptions: PluginsOptions = {}) {
       })
     },
     update(el, binding: VNodeDirective, vnode: VNode) {
-      if (el._autoNumericElement.valueOnFocus !== binding.value.bind) { // 通过代码直接修改了binding.value.bind会进入这个逻辑
+      if (el._autoNumericElement.lastVal !== binding.value.bind) { // 通过代码直接修改了binding.value.bind会进入这个逻辑
         let targetElement = checkElementType(el, vnode)
         let oldValue = getVNodeValue(vnode)
         let options = getOptions(binding, outerOptions)
 
-        el._autoNumericElement = new AutoNumeric(targetElement, oldValue, options.numricOptions)
+        el._autoNumericElement.remove()
+        el._autoNumericElement = new AutoNumeric(targetElement, options.numricOptions)
       }
     },
   })
