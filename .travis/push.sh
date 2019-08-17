492251327f100b12d3c27db0d943241df7a45b04
awam.m.wang <awam.m.wang@cn.pwc.com>
2019-08-17 14:47:24 +0800

    build: 发布脚本问题修改

--- .travis/push.sh
+++ .travis/push.sh
@@ -36,7 +36,7 @@ upload_files() {
 npm_publish() {
   echo "$TRAVIS_COMMIT_MESSAGE"
   echo $version_pattern
-  if [[ $TRAVIS_COMMIT_MESSAGE =~ $version_pattern ]] then
+  if [[ $TRAVIS_COMMIT_MESSAGE =~ $version_pattern ]]; then
     echo "Npm publish ${TRAVIS_COMMIT_MESSAGE}..."
     npm publish
   fi
